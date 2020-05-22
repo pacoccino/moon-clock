5ebece5801dce6cbde9bcceb3943c1f9216ed7c8
Pacien Boisson <pakokrew@gmail.com>
2020-05-22 11:57:25 +0200

    PWA

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,20 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Moon Clock</title>
+
+        <meta charSet='utf-8'/>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+        <meta name='viewport'
+              content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
+        <meta name='description' content='Moon clock and phases'/>
+        <meta name='keywords' content='moon,phases,cycles,full moon,waxing,crescent'/>
+
+        <link rel="manifest" href="/manifest.json"/>
         <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon-16x16.png' type='image/png' sizes='16x16'/>
+        <link rel='icon' href='/favicon-32x32.png' type='image/png' sizes='32x32'/>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+        <meta name="theme-color" content="#317EFB"/>
       </Head>
 
       <main>
@@ -42,17 +55,6 @@ export default function Home() {
         </div>
         <p className="days-info">Days since new moon: {daysSinceNew.integerValue(BN.ROUND_FLOOR).toFixed()}</p>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -96,40 +98,6 @@ export default function Home() {
         .days-info {
           color: white;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-        
-        
-        .logo {
-          height: 1em;
-        }
-
-
       `}</style>
 
       <style jsx global>{`
