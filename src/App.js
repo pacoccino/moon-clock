7694cf37eb22061c737d0642764b1377767ff81c
Pacien Boisson <pakokrew@gmail.com>
2020-06-01 18:14:53 +0200

    next to react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head'
-import moon from '../lib/moon';
-import BN from 'bignumber.js';
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
+import BN from 'bignumber.js';
+import moon from './lib/moon';
+
+import './App.css';
+
+function App() {
   const [moonState, setMoonState] = useState(() => moon.getState());
 
   useEffect(() => {
@@ -15,41 +17,10 @@ export default function Home() {
 
   }, []);
 
-  // Debug stuff
-
-  // const [a,b] = useState(0);
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     b(aa => ((aa+1)%8));
-  //   }, 1000)
-  // }, []);
-  // const currentPhase = BN(a);
-  // const { daysSinceNew } = moonState;
-
   const { currentPhase, daysSinceNew } = moonState;
-
-  console.log(currentPhase.toFixed(), daysSinceNew.toFixed());
 
   return (
     <div className="container">
-      <Head>
-        <title>Moon Clock</title>
-
-        <meta charSet='utf-8'/>
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
-        <meta name='viewport'
-              content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
-        <meta name='description' content='Moon clock and phases'/>
-        <meta name='keywords' content='moon,phases,cycles,full moon,waxing,crescent'/>
-
-        <link rel="manifest" href="/manifest.json"/>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel='icon' href='/favicon-16x16.png' type='image/png' sizes='16x16'/>
-        <link rel='icon' href='/favicon-32x32.png' type='image/png' sizes='32x32'/>
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
-        <meta name="theme-color" content="#317EFB"/>
-      </Head>
-
       <main>
         <div className="moon-container">
           <svg width="100%" height="100%" viewBox="0 0 100 100">
@@ -71,59 +42,6 @@ export default function Home() {
           Days since new moon: {daysSinceNew.integerValue(BN.ROUND_FLOOR).toFixed()}
         </p>
       </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          flex: 1;
-          display: flex;
-           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: #20212d;
-          width: 100%;
-        }
-        
-        .moon-container {
-          margin: 5rem 0;
-          width: 70%;
-          max-width: 400px;
-          position: relative;
-        }
-        .moon-container:after {
-          content: "";
-          display: block;
-          padding-bottom: 100%;
-          position: absolute;
-        }
-
-        .days-info {
-          color: white;
-          text-align: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
 
       <svg width={0} height={0}>
         <defs>
@@ -165,5 +83,7 @@ export default function Home() {
         </defs>
       </svg>
     </div>
-  )
+  );
 }
+
+export default App;
